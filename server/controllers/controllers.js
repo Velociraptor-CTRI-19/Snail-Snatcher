debdfae2dde82680a9e6546412cbc69272684d60
Joseph Paul <joepubacc@gmail.com>
2023-10-25 22:50:35 -0400

    create middleware functions in controllers file

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -3,7 +3,55 @@ const db = require('../models/Models');
 const controller = {};
 
 controller.addItems = (req, res, next) => {
-  
+  const { item, description, price } = req.body;
+
+  const text = 
+    `INSERT INTO sellers (item, description, price)
+    VALUES ('${item}', '${description}', ${price});`;
+
+  db.query(text)
+    .then((result) => {
+      next();
+    })
+    .catch((error) => next({
+      log: 'An error occurred in controller.addItems',
+      status: 500,
+      message: {err: error}
+    }));
 };
+
+controller.buyersDisplay = (req, res, next) => {
+  
+  const text = 'SELECT description, item, price FROM sellers;';
+
+  db.query(text)
+    .then((result) => {
+      res.locals.dbInfo = result.rows;
+      next();
+    })
+    .catch((err) => next({
+      log: 'An error occurred in controller.buyersDisplay',
+      status: 500,
+      message: {err}
+    }));
+};
+
+controller.purchaseItem = (req, res, next) => {
+  const { item } = req.body;
+
+  const text = 'DELETE FROM sellers WHERE item = $1;';
+
+  const values = [item];
+  db.query(text, values)
+    .then((response) => {
+      res.locals.message = {message: `${item} was purchased`};
+      next();
+    })
+    .catch((err) => next({
+      log: 'An error occurred.',
+      status: 500,
+      message: {err}
+    }));
+}
 
 module.exports = controller;
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const path = require("path");
 const PORT = 3000;
 
 const db = require("./models/Models.js");
+const controller = require("./controllers/controllers.js");
 
 //handle parsing request body
 app.use(express.json());
@@ -24,67 +25,18 @@ app.use(express.json());
 // app.use("/", express.static(path.join(__dirname, "../client/index.html")));
 
 //post request to add items
-app.post("/sellers/addItems", (req, res) => {
-  const { item, description, price } = req.body;
-  // console.log('req.body', req.body);
-  // console.log("got into sellers/addItems");
-
-  const text = `
-    INSERT INTO sellers (item, description, price)
-    VALUES ('${item}', '${description}', ${price});
-    `;
-
-  db.query(text)
-    .then((result) => {
-      // console.log("Item: ", result);
-      res.status(200).json({ message: "Item added" });
-    })
-    .catch((error) => {
-      console.error("Error with adding item in server.js", error);
-      res.status(500).json({ error: "Server error" });
-    });
+app.post("/sellers/addItems", controller.addItems, (req, res) => {
+  res.status(200).json({ message: "Item added" });
 });
 
 //get request to display
-app.get("/buyers/display", (req, res) => {
-  // console.log("entered buyers/display");
-  const text = `
-    SELECT description, item, price FROM sellers;
-    `;
-
-  db.query(text)
-    .then((result) => {
-      // console.log("Result: ", result);
-      // console.log("Result of rows: ", result.rows);
-      res.status(200).json(result.rows);
-    })
-    .catch((err) => {
-      res
-        .status(500)
-        .json({ message: "error when retrieving data from database" });
-    });
+app.get("/buyers/display", controller.buyersDisplay, (req, res) => {
+  res.status(200).json(res.locals.dbInfo);
 });
 
 //delete request to purchaseItem
-app.delete("/buyers/purchaseItem", (req, res) => {
-  console.log("entered buyers/purchaseItem");
-  const { item } = req.body;
-  // console.log("item: ", item);
-
-  const text = `
-        DELETE FROM sellers
-        WHERE item = $1;
-    `;
-
-  const values = [item];
-  db.query(text, values)
-    .then((response) => {
-      // console.log('.then ~ response:', response);
-      res.status(200).json({ message: `${item} was purchased` });
-    })
-    .catch((err) => {
-      res.status(500).json({ message: `unable to purchase ${item}`})
-    });
+app.delete("/buyers/purchaseItem", controller.purchaseItem, (req, res) => {
+  res.status(200).json(res.locals.message);
 });
 
 // global error handler
